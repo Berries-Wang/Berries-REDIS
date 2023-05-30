9d7200b67df20d64cb16528e965931a93621b850
Wei.Wang <1849553578@qq.com>
2023-05-30 08:37:11 +0800

    feat>Redis>Cluster学习

---
+++ 005.REDIS-KERNEL/004.REDIS_CLUSTER/000.REDIS_CLUSTER_CONFIG/REDIS_CLUSTER_NODE_1/start_node_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo 'Redis Server 启动'
+if [ $# -ne 1 ];then
+    echo "请指定参数"
+    exit 1;
+fi
+PATHDIR=`pwd`
+echo "当前目录:`pwd`"
+if [[ $1 = "I" ]];then
+    echo "第一次启动..."
+    cp REDIS_CLUSTER_NODE_1.conf REDIS_CLUSTER_NODE_1_copy.conf
+    rm -f ./*.aof && rm -f ./*.rdb
+elif [[ $1 = "A" ]];then
+    echo "重新上线..."
+else
+    echo "未知选项..."
+    exit 1;
+fi
+
+"./../../../../001.REDIS_SOURCE_CODE/redis-6.2.5/src/redis-server" ${PATHDIR}/REDIS_CLUSTER_NODE_1_copy.conf
--- 005.REDIS-KERNEL/004.REDIS_CLUSTER/README.md
+++ 005.REDIS-KERNEL/004.REDIS_CLUSTER/README.md
@@ -1,1 +1,7 @@
 # Redis 集群
+
+## 开启集群模式
+&nbsp;&nbsp;一个Redis集群节点就是一个运行在集群模式下的Redis服务器，Redis服务器在启动时会根据cluster-ebabled配置选项是否为yes来决定是否开启服务器的集群模式.
+```txt
+    cluster-enabled yes
+```
